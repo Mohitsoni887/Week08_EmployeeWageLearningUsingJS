3ebaa45befb035cdf41eb2c7e4bfa27a66c0d4d1
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-03-05 10:52:44 +0530

    [Mohit Soni] Add. employee daily wage calculation

--- Employee_Wages/Main.js
+++ Employee_Wages/Main.js
@@ -1,12 +1,24 @@
-// Constants
-const PRESENT = 1;
+const WAGE_PER_HOUR = 20;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
 
-// Generate a random number (0 or 1)
-let empCheck = Math.floor(Math.random() * 2); // Randomly 0 or 1
+let workType = Math.floor(Math.random() * 3);
+let workHours = 0;
 
-// Check Employee Status
-if (empCheck === PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
+switch (workType) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work");
+        break;
 }
+
+let dailyWage = workHours * WAGE_PER_HOUR;
+console.log("Daily Wage: $" + dailyWage);
